e45dfcd5101c3ff6d08df3493e4fa1acf58fc943
Emir Önder Demircan <62562622+emironderdemircan@users.noreply.github.com>
2021-12-07 02:04:57 +0300

    Admin
    Admin paneli ve database i oluşturuldu

---
+++ whichmovieisgood/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using whichmovieisgood.Models.Manager;
+
+namespace whichmovieisgood.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
